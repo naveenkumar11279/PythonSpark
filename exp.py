86ccf8dc4efaa2573fd44dfedc856720b80380a1
Naveen Kumar <naveenkumar10272@gmail.com>
2024-11-20 19:42:52 +0530

    commiting opening file with try and expect block

--- exp.py
+++ exp.py
@@ -1,3 +1,11 @@
-with open("D:\data\day1db.txt","r") as file:
-    content = file.read()
-    print(content)
+
+filepath = "D:\data\day1db.txt"
+try:
+    file = open(filepath)
+except Exception as e:
+    print(e)
+else:
+    contents = file.read()
+    print(contents)
+finally:
+    print(f"Closing the file :{file.name}")
