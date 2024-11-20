1294275ca61ee30be99a4c3d1fdf39b52460a3ce
Naveen Kumar <naveenkumar10272@gmail.com>
2024-11-20 19:21:50 +0530

    Initial Commit for the file insta_dummy

---
+++ insta_dummy.py
@@ -0,0 +1,32 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import *
+from datetime import datetime
+from functools import reduce
+import json
+import requests
+from pyspark.sql.types import *
+# Words count program in pyspark
+conf = SparkConf().setAppName("Practice").setMaster("local[*]")
+sc = SparkContext(conf=conf)
+spark = SparkSession.builder\
+    .appName("Myapp")\
+    .master("local[*]")\
+         .getOrCreate()
+print("Application stating from here==>")
+
+Insta_raw_df = spark.read.format("csv").option("header","true").load(r'D:\Data\insta_dummy.csv')
+Insta_raw_df.show(truncate=False)
+
+currentdate = "2024-11-04 08:25:00"
+
+timestampde = Insta_raw_df.withColumn("Timestamp", to_timestamp(lit(col("Timestamp"))))
+ladata = timestampde.filter(
+    col("Timestamp") > date_sub(to_date(lit(currentdate)),1)
+)
+
+timestampde.show()
+print(currentdate)
+print(timestampde.printSchema())
+print("updating")
+
